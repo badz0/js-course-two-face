f566eb0305c071189cfc349da8bdee10e89e6fd2
Bogdan <bogdan.muranets@gmail.com>
2020-08-20 21:41:12 +0300

    62-ajax

--- 62-ajax/index.js
+++ 62-ajax/index.js
@@ -1,114 +1,138 @@
-const KEY = 'e0dcd22de287c749b30f3ebe066f87751f2ede6f42e4148ff47a6be2411c392b'
-const BASE = 'https://gorest.co.in/public-api'
+// /users/{id}/posts/{post_id}/comments/
+// /users/{id}/posts/{post_id}/comments?page=3
 
 
-async function getUsers(nameFilter) {
-  try {
-    // headers: {'Authorization': 'Bearer ' + KEY},
-    const config = {
-      params: {name: nameFilter, page: 85}
-    };
 
-    const res = await axios.get(BASE + '/users', config);
+const BASE = 'https://gorest.co.in/public-api/';
+const KEY = 'e0dcd22de287c749b30f3ebe066f87751f2ede6f42e4148ff47a6be2411c392b';
 
-    return res.data.data;
-  } catch (err) {
-    console.log('something bad happened');
-    return [];
-  }
-}
-
-function renderUsers(users) {
-  const container = document.querySelector('.users');
-  container.innerHTML = '';
-  const fragment = new DocumentFragment();
-  users.forEach(user => {
-    const userDiv = document.createElement('div');
-    userDiv.classList.add('user');
-    userDiv.innerHTML = `
-      <h4>${user.name}</h4>
-      <h5>${user.email}</h5>
-    `;
-    const remove = document.createElement('button');
-    remove.innerText = 'X';
-    remove.className = 'user__remove';
-    remove.addEventListener('click', () => {
-      removeUser(user.id);
-    });
-    userDiv.append(remove);
-    fragment.append(userDiv);
-  })
-  container.append(fragment);
-}
-const instance = axios.create({
-  headers: {'Authorization': 'Bearer ' + KEY}
-});
-
-function createUser(name, email) {
-  // headers: {'Authorization': 'Bearer ' + KEY},
+async function getUsers(page) {
   const config = {
-    method: 'POST',
-    data: {
-      name: name,
-      email,
-      status: 'Inactive',
-      gender: 'Male'
+    method: 'GET',
+    url: BASE + 'users',
+    params: {
+      page: page
     }
   };
 
-  return instance(BASE + '/users', config).catch((err) => {
-    console.log('errr', err);
-  });
+  try {
+    const res = await axios(config);
+    return res.data.data;
+  } catch {
+    console.log('something went wrong');
+  };
+
+  // return axios(config).then((res) => {
+  //   return res.data.data;
+  // }).catch(() => {
+  //   console.log('something went wrong');
+  // });
 }
 
-document.filterForm.elements.filterApply.addEventListener('click', async function(event) {
-  const val = document.filterForm.elements.filterInput.value;
-  if (!val) return;
-  
-  const users = await getUsers(val);
-  document.filterForm.elements.filterInput.value = '';
-  renderUsers(users);
-});
+function renderUsers(users = []) {
+  const container = document.querySelector('.users');
+  container.innerHTML = '';
 
-const emailEl = document.querySelector('#email');
-const btnCreate = document.querySelector('#create');
-
-btnCreate.addEventListener('click', () => {
-  if (document.createUser.name.value && emailEl.value) {
-    createUser(document.createUser.name.value, emailEl.value)
-      .then(() => {
-        document.createUser.name.value = '';
-        emailEl.value = '';
-      });
-  }
-});
-
-getUsers().then((users) => {
-  renderUsers(users);
-});
-// getUsers().then(renderUsers)
-
-
-
-
-
-const containerEl = document.querySelector('.details');
-containerEl.addEventListener('click', function(event) {
-  if (this !== event.target) return;
-  this.classList.remove('shown')
-});
-
-function getDetails(movie) {
-  axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits` + key).then(res => {
-    console.log('res: ', res);
-    containerEl.classList.add('shown');
-    console.log('containerEl.firstChild: ', containerEl.firstChild);
-    containerEl.firstElementChild.innerHTML = `
-      <img class="movie__img" src="https://image.tmdb.org/t/p/original${movie.poster_path}">
-      <h4>${movie.title}</h4>
-      <p>
-        ${res.data.cast.map(item => item.character).join(', ')}
-      </p>
+  users.forEach(user => {
+    const userElement = document.createElement('div');
+    userElement.className = 'user';
+    userElement.innerHTML = `
+      <h3>${user.name}</h3>
+      <h4>${user.email}</h4>
     `;
+    container.append(userElement);
   });
 }
+
+
+function createUser() {
+  const name = document.createUser.name.value;
+  const email = document.createUser.email.value;
+  if (!name || !email) return;
+  
+  axios.post(BASE + 'users', {
+    name,
+    email,
+    gender: 'Female',
+    status: 'Active'
+  }, {
+    headers: {'Authorization': 'Bearer ' + KEY},
+  }).then((res) => {
+    document.createUser.name.value = '';
+    document.createUser.email.value = '';
+
+    saveUser(res.data.data.id);
+  }).catch(() => {
+    console.log('ooops');
+  });
+}
+
+function saveUser(id) {
+  if (!id) return;
+
+  let users = localStorage.getItem('users');
+  if (!users) users = [];
+  else users = JSON.parse(users);
+
+  users.push(id);
+
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+
+function paginationInit() {
+  let currentPage = 1;
+  document.getElementById('prev').addEventListener('click', () => {
+    if (currentPage < 1) return;
+    getUsers(--currentPage).then(renderUsers);
+  });
+  document.getElementById('next').addEventListener('click', () => {
+    getUsers(++currentPage).then(renderUsers);
+  });
+}
+
+function displaySavedUsers() {
+  let users = localStorage.getItem('users');
+  if (!users) return;
+  users = JSON.parse(users);
+
+  Promise.all(users.map(id => {
+    return axios.get(`${BASE}users/${id}`).then(res => {
+      if (res.data.data.id) {
+        return res.data.data;
+      } else return Promise.reject('used wansnt found');
+    });
+  })).then(renderUsers).catch((err) => {
+    console.log('err: ', err);
+  });
+}
+
+document.querySelector('#displaySaved')
+  .addEventListener('click', displaySavedUsers)
+
+document.getElementById('create')
+  .addEventListener('click', createUser)
+
+
+paginationInit();
+
+// getUsers().then(renderUsers);
+
+async function fetchUsers(page = 1) {
+  try {
+    const res = await fetch(`${BASE}users?page=${page}`);
+    if (res.ok) {
+      const body = await res.json();
+      return body.data;
+    } else {
+      throw new Error('http error')
+    }
+  } catch(err) {
+    console.log('err: ', err);
+    console.log('something went wrong');
+  };
+}
+
+
+fetchUsers().then(renderUsers);
+
